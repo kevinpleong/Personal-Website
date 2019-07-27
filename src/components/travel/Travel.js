e93f909245d4b87b0edd264d0429748163dd7012
Kevin L <kevinpleong@gmail.com>
2019-07-27 00:24:54 -0700

    adding gallery

--- src/components/travel/Travel.js
+++ src/components/travel/Travel.js
@@ -1,13 +1,64 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Australia from './Australia.png'
+import Belgium from './Belgium.HEIC'
+import England from './Big Ben England.JPG'
+import Boston from './Boston.jpg'
+import Chamonix from './Chamonix France.JPG'
+import greatWallofChina from './China Great Wall.jpeg'
+import harbinIceandSnowSculptureFestivalChina from './China Ice Glacier.jpeg'
+import disney-Paris from './Disney Paris.JPG'
+import Ecuador from './Ecuador.png'
+import Galapagos from './Galapagos.png'
+import Germany from './Germany.JPG'
+import England from './Harry Potter England.JPG'
+import Hawaii from './hawaii'
+import New-Zealand from './Hobbit New Zealand.jpg'
+import Thailand from './James Bond Island Thailand.heic'
+import potStickersJapan from './Japan.png'
+import japanStreetView from './Japan2.png'
+import Korea from './Korea.png'
+import LA from './LA.jpg'
+import Malaysia from './Malaysia.png'
+import newOrleans from './New Orleans.png'
+import newYork from './New York.HEIC'
+import paraglidingFrance from './Paragliding France.png'
+import italyRome from './Rome Italy Colosseum.JPG'
+import Seattle from './Seattle.HEIC'
+import Singapore from './Singapore.png'
+import Taiwan from './Taipei 101 Taiwan.jpeg'
+import Giants-Virginia from './Virginia.jpg'
+import Eating-Virginia from './Virginia2.JPG'
+import Tioman Malasia from './Tioman Malaysia 101514.jpeg'
+import Singapore from './Singapore 101214.jpeg'
+import Phuket from './Phuket Elephant Ride 032919.jpeg'
+import morrocoCamels from './Morocco 062715.jpeg'
+import sydneyBridgeOperaHouse from './Sydney Bridge and Opera House.jpeg'
+import scudaDivingCebu from './KTD Scuba Cebu.jpeg'
+import zooJapan from './zoo-Japan.jpeg'
+import DMZKorea from './DMZ-Korea 040412.jpeg'
+import london-Bridge from './London-bridge.jpeg'
+import Japan Train.jpeg from './'
+import notreDameParis from './Notre Dame Paris.jpeg'
+import middleOfTheWorldEcuador from './Equador 033114.jpeg'
+import galapagos from './Galapagos 040414.jpeg'
+import rockOfGibraltar from './Rock of Gibraltar 062615.jpeg'
+import sydneyHarborBridge from './Sydney Harbor Bridge 072714.jpeg'
+import Spain from './Sagrada Familia Church Barcelona 070115.jpeg'
+import shuttleLaunchFlorida  from './Space Shuttle Launch.jpeg'
+import sandCastleBorocay from './Borocay Sand Castle.jpeg'
+import parasailing from './Parasailing Borocay Philippines.jpeg'
+
 
 export default class Travel extends React.Component {
   render() {
     return (
       <div class="container">
-      <h1>Travel</h1>
-      <p>I enjoy travelling all over the world; I love to travel and I have been to 6 out of the 7 contents; I still have not visitted Antarctica. I have travelled domestically to Hawaii, Virginia, New York, LA, Seattle Oregon, and New Orleans. Internationally I have been to Japan, Philippines, Korea, Malaysia, Singapore, indonesia, England, France, Germany, Belgium, Italy, Australia, New Zealand, Galapagos, Ecuador, Vietnam, China, Thailand, and Taiwan.</p>
-    </div>
-  );
+      <h1>Travels</h1>
+        <img src="/w3images/lights.jpg" alt="Lights" style="width:100%">
+         <div class="caption text-center">
+           <p class="text-center">text</p>
+         </div>
+        };
 
-  }
-}
+        ReactDOM.render(<Demo5/>, document.getElementById('demo5'));
